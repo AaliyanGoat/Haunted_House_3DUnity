579beaf52c7f9b8d291c9811cf70ce2889ba5bf7
AaliyanGoat <muhammadaaliyanatif77@gmail.com>
2026-08-05 03:26:44 +0500

    Initial commit

---
+++ Assets/_3DStealthGame/scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private List<string> m_OwnedKeys = new List<string>();
+    AudioSource m_AudioSource;
+    Animator m_Animator;
+    public InputAction MoveAction;
+
+    public float walkSpeed = 1.0f;
+    public float turnSpeed = 20f;
+
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+public void AddKey(string keyName)
+{
+    m_OwnedKeys.Add(keyName);
+}
+
+public bool OwnKey(string keyName)
+{
+    return m_OwnedKeys.Contains(keyName);
+}
+    void Start ()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+        m_Rigidbody = GetComponent<Rigidbody> ();
+        MoveAction.Enable();
+        m_Animator = GetComponent<Animator> ();
+    }
+
+    void FixedUpdate ()
+    {
+        var pos = MoveAction.ReadValue<Vector2>();
+        
+        float horizontal = pos.x;
+        float vertical = pos.y;
+        
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize ();
+
+        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
+bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
+bool isWalking = hasHorizontalInput || hasVerticalInput;
+m_Animator.SetBool ("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation (desiredForward);
+        
+        m_Rigidbody.MoveRotation (m_Rotation);
+        m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * walkSpeed * Time.deltaTime);
+        if (isWalking)
+{
+    if (!m_AudioSource.isPlaying)
+    {
+        m_AudioSource.Play();
+    }
+}
+else
+{
+    m_AudioSource.Stop();
+}
+    }
+}
